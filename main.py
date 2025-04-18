2d07be1043c6188b11cf784e7e9bd7d710351229
Dov Cohen <idcohen@ymail.com>
2025-04-17 23:33:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# remote sensor
+from microbit import *
+import radio
+radio.config(group=23)
+radio.on()
+
+
+while True:
+    if button_a.was_pressed():    
+ #      while True
+        for x in range(5):
+            display.clear()
+            C = temperature()
+            F = 32 + 9/5*C
+            msg = str(C)
+            radio.send(msg)
+            sleep(5)
+    if button_b.was_pressed():  
+        msg = ""
+        radio.send(msg)
